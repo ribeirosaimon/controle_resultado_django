04230e8a0b4becf482fe34b7f73a4dbd339c0693
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2021-03-07 20:20:34 -0300

    Update settings.py

--- controle_resultado/settings.py
+++ controle_resultado/settings.py
@@ -130,7 +130,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SITE_ID = 1
 
